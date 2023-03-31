ff8a16d1ec2325b9fccfbbc043fe2598c1acab7d
Agneya T <agneyat2@gmail.com>
2023-03-31 14:46:31 -0400

    removed useless files and added registration protection
    you cant make duplicate accounts with the same account and if you try theres a small blurb that tells u that an acc with that email already exists.

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import bcrypt
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from models import Accounts, Students
-from __init__ import app
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fa1(0nwar3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
+
+db.init_app(app)
+
+class Accounts(db.Model):
+	__tablename__ = 'accounts'
+	account_id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(200),nullable=False)
+	password = db.Column(db.String(200),nullable=False)
+
+class Students(db.Model):
+	__tablename__ = 'students'
+	student_id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200),nullable=False)
+	goals = db.Column(db.String(200),nullable=False)
 
 @app.route("/")
 def login():
@@ -17,7 +34,8 @@ def register():
 		user = Accounts.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
 		
 		if user: # if a user is found, we want to redirect back to signup page so user can try again
-			return redirect(url_for('auth.signup'))
+			flash('Email address already exists')
+			return render_template("register.html")
 
 		account_user = Accounts(username=username, password=password)
 
