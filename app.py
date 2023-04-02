7cf9bbf9f53c8688f8e0d36b9d7547d6884daa07
Agneya T <agneyat2@gmail.com>
2023-04-02 15:16:37 -0400

    user session implemented, navbar upgraded
    databased encrypted, navbar reactive, added flavicon, added logo

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import bcrypt
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fa1(0nwar3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
 
 db.init_app(app)
 
-class Accounts(db.Model):
+login_manager = LoginManager()
+login_manager.login_view = 'auth.signin'
+login_manager.init_app(app)
+
+class Accounts(UserMixin, db.Model):
 	__tablename__ = 'accounts'
 	account_id = db.Column(db.Integer, primary_key=True)
 	callname = db.Column(db.String(200),nullable=False)
 	username = db.Column(db.String(200),nullable=False)
 	password = db.Column(db.String(200),nullable=False)
+
+	def get_id(self):
+		return (self.account_id)
 
 class Students(db.Model):
 	__tablename__ = 'students'
@@ -23,11 +32,24 @@ class Students(db.Model):
 	name = db.Column(db.String(200),nullable=False)
 	goals = db.Column(db.String(200),nullable=False)
 
-@app.route("/")
+@login_manager.user_loader
+def load_user(account_id):
+    return Accounts.query.get(int(account_id))
+
+main = Blueprint('main', __name__)
+auth = Blueprint('auth', __name__)
+
+@main.route("/")
 def home():
 	return render_template("login.html")
 
-@app.route("/signin", methods=('GET', 'POST'))
+@main.route("/accounts")
+@login_required
+def account():
+	accounts = Accounts.query.all()
+	return render_template('accounts.html',accounts=accounts)
+
+@auth.route("/signin", methods=('GET', 'POST'))
 def signin():
 	if request.method == 'POST':
 		username = request.form["logusername"]
@@ -39,12 +61,13 @@ def signin():
 			flash('Please check your login details and try again.')
 			return render_template("login.html")
 		else:
+			login_user(user)
 			accounts = Accounts.query.all()
-			return render_template('accounts.html',accounts=accounts)
+			return redirect(url_for('main.account'))
 
 	return render_template("login.html")
 
-@app.route("/register", methods=('GET', 'POST'))
+@auth.route("/register", methods=('GET', 'POST'))
 def register():
 	if request.method == 'POST':
 		callname = request.form["regcallname"]
@@ -56,17 +79,20 @@ def register():
 		if user:
 			flash('Email address already exists')
 			return render_template("register.html")
-
 		account_user = Accounts(callname=callname, username=username, password=generate_password_hash(password, method='sha256'))
 
 		db.session.add(account_user)
 		db.session.commit()
 	return render_template("register.html")
 
-@app.route("/accounts")
-def account():
-	accounts = Accounts.query.all()
-	return render_template('accounts.html',accounts=accounts)
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+app.register_blueprint(auth)
+app.register_blueprint(main)
 
 if __name__ == '__main__':
 	app.run(debug=True)
